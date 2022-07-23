1ba8909642a243e4d451d2f163237bbba92fd1b7
James Parker <j1mb0jay@jimbojay.com>
2022-07-23 14:08:24 +0100

    auth

--- src/functions/api/command/submit/index.ts
+++ src/functions/api/command/submit/index.ts
@@ -12,13 +12,18 @@ export const onRequestPost: PagesFunction<{ TOKEN: string }> = async ({ env, req
             if (payload.type === "view_submission") {
                 const id = payload.view.callback_id;
                 const meta = JSON.parse(payload.view.private_metadata);
-                const state = payload.view.state.value;
+                const state = payload.view.state.values;
                 if (meta.meta.verify && meta.meta.channel) {
-                    await fetch('https://slack.com/api/chat.postMessage',
+
+                    const value = JSON.stringify({ meta: meta, values: state, user: payload.user });
+                    console.log(value);
+
+                    const response = await fetch('https://slack.com/api/chat.postMessage',
                         {
                             method: "POST",
                             headers: {
-                                "Content-Type": "application/json;charset=UTF8"
+                                "Content-Type": "application/json;charset=UTF8",
+                                "Authorization": `Bearer ${env.TOKEN}`
                             },
                             body: JSON.stringify({
                                 channel: "C4QB62S86",
@@ -37,7 +42,7 @@ export const onRequestPost: PagesFunction<{ TOKEN: string }> = async ({ env, req
                                                 type: "button",
                                                 action_id: "yes",
                                                 style: "primary",
-                                                value: JSON.stringify({ meta: meta, values: state, user: payload.user }),
+                                                value: value,
                                                 text: {
                                                     type: "plain_text",
                                                     text: "Yes",
@@ -48,7 +53,7 @@ export const onRequestPost: PagesFunction<{ TOKEN: string }> = async ({ env, req
                                                 type: "button",
                                                 action_id: "no",
                                                 style: "danger",
-                                                value: JSON.stringify({ meta: meta, values: state, user: payload.user }),
+                                                value: value,
                                                 text: {
                                                     type: "plain_text",
                                                     text: "No",
@@ -59,14 +64,21 @@ export const onRequestPost: PagesFunction<{ TOKEN: string }> = async ({ env, req
                                     }
                                 ]
                             })
-                        })
+                        });
+                    console.log(await response.json());
                 }
                 console.log(id, meta);
             }
             else if (payload.type === "block_actions") {
                 let message = ""
                 if (payload.actions[0].action_id === "yes") {
-                    message = `@${user} accepted the request \`${payload.actions[0].value}\``
+                    message = `@${user} accepted the request \`${payload.actions[0].value}\``;
+
+                    await fetch("https://www.dotnethelp.co.uk/api/post/7d9a4ff5-ac11-4aaf-b0d2-4504782cfa74",
+                    {
+                        method: "POST",
+                        body: payload.actions[0].value,
+                    });
                 }
                 else {
                     message = `@${user} denied the request \`${payload.actions[0].value}\``
